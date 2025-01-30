da96e6f5147668dca82bd4aa247cb5ffc1298042
Vlad796 <gonevlad02@gmail.com>
2025-01-30 17:10:35 +0300

    Initial Commit

---
+++ myMapApp/myMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMapAppApp.swift
+//  myMapApp
+//
+//  Created by Влад Варламов on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct myMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
